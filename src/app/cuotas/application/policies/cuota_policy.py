497ea4528abf916215a00d225998456d3c72a504
Miguel Angel Vigo Bazm <vigobaz@hotmail.com>
2025-10-24 10:43:28 +0200

    Feat(cuotas): Implementar caso de uso para crear temporada

--- src/app/cuotas/application/policies/cuota_policy.py
+++ src/app/cuotas/application/policies/cuota_policy.py
@@ -1,2 +1,6 @@
+from src.app.cuotas.application.dtos import UsuarioPolicyDTO
+from src.app.users.domain.value_objects import Rol
+
 class CuotaPolicy:
-    pass
+    def es_administrador(self, current_user: UsuarioPolicyDTO) -> bool:
+        return current_user.rol == Rol.ADMIN
