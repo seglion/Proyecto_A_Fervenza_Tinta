5955ef6bd1ad502880a8821efd1d023f971e1828
Miguel Angel Vigo Bazm <vigobaz@hotmail.com>
2025-10-26 15:55:45 +0100

    Feat: Implementar caso de uso para generar informe de pendientes

--- tests/app/cuotas/application/repositories/test_i_cuota_repository.py
+++ tests/app/cuotas/application/repositories/test_i_cuota_repository.py
@@ -15,3 +15,24 @@ def test_all_methods_are_abstract():
         class ConcreteCuotaRepository(ICuotaRepository):
             pass
         ConcreteCuotaRepository()
+
+def test_get_usuarios_pendientes_por_temporada_is_abstract_method():
+    with pytest.raises(TypeError):
+        class ConcreteCuotaRepository(ICuotaRepository):
+            async def listar_todas(self):
+                pass
+            async def buscar_por_usuario_y_temporada(self, usuario_id, temporada_id):
+                pass
+            async def guardar(self, cuota):
+                pass
+            async def buscar_por_id_con_detalle(self, cuota_id):
+                pass
+            async def buscar_por_id(self, cuota_id):
+                pass
+            async def actualizar(self, cuota):
+                pass
+            async def buscar_por_usuario_id_completadas(self, usuario_id):
+                pass
+            async def ha_pagado_cuota_alta_antes(self, user_id):
+                pass
+        ConcreteCuotaRepository()
