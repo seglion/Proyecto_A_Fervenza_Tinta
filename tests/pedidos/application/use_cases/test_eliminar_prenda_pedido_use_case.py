3f36ce5c79bb6bcacbfc3692cee59a711e2d3b38
Miguel Angel Vigo Bazm <vigobaz@hotmail.com>
2025-11-04 11:21:01 +0100

    feat(pedidos): implementa UC3 EliminarPrendaPedidoUseCase

---
+++ tests/pedidos/application/use_cases/test_eliminar_prenda_pedido_use_case.py
@@ -0,0 +1,4 @@
+from src.app.pedidos.application.use_cases.eliminar_prenda_pedido_use_case import EliminarPrendaPedidoUseCase
+
+def test_eliminar_prenda_pedido_use_case_existe():
+    assert EliminarPrendaPedidoUseCase is not None
