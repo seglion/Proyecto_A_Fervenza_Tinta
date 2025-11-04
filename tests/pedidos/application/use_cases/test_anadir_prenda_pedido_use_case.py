876fb5dcf3b009ef7f25c762bc480a4acc0ddf81
Miguel Angel Vigo Bazm <vigobaz@hotmail.com>
2025-11-04 10:46:48 +0100

    feat(pedidos): implementa UC2 AnadirPrendaPedidoUseCase

---
+++ tests/pedidos/application/use_cases/test_anadir_prenda_pedido_use_case.py
@@ -0,0 +1,4 @@
+from src.app.pedidos.application.use_cases.anadir_prenda_pedido_use_case import AnadirPrendaPedidoUseCase
+
+def test_anadir_prenda_pedido_use_case_existe():
+    assert AnadirPrendaPedidoUseCase is not None
