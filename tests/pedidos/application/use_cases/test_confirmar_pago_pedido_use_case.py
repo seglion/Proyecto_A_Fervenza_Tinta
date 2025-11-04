49b6ecca81b1ca96474265b2860fdac7e186ed3c
Miguel Angel Vigo Bazm <vigobaz@hotmail.com>
2025-11-04 11:29:27 +0100

    feat(pedidos): implementa UC4 ConfirmarPagoPedidoUseCase

---
+++ tests/pedidos/application/use_cases/test_confirmar_pago_pedido_use_case.py
@@ -0,0 +1,4 @@
+from src.app.pedidos.application.use_cases.confirmar_pago_pedido_use_case import ConfirmarPagoPedidoUseCase
+
+def test_confirmar_pago_pedido_use_case_existe():
+    assert ConfirmarPagoPedidoUseCase is not None
