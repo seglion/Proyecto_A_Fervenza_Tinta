edba8137d6893984361d22f65e6984f7d329f7fb
Miguel Angel Vigo Bazm <vigobaz@hotmail.com>
2025-11-04 12:55:12 +0100

    feat(pedidos): implementa UC9 VerDetallePedidoAdminUseCase

---
+++ tests/pedidos/application/use_cases/test_ver_detalle_pedido_admin_use_case.py
@@ -0,0 +1,4 @@
+from src.app.pedidos.application.use_cases.ver_detalle_pedido_admin_use_case import VerDetallePedidoAdminUseCase
+
+def test_ver_detalle_pedido_admin_use_case_existe():
+    assert VerDetallePedidoAdminUseCase is not None
